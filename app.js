420493e1f793d7b2c08a32469c9e7598c10a230d
danieldannitz <danielgnlmenezes@gmail.com>
2022-06-07 21:06:44 -0300

    add hours and minutes

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
   const secondsAngle = (seconds / 60) * 360 + 90;
-  const secondHands = document.querySelector(".second-hand");
-  secondHands.style.transform = `rotate(${secondsAngle}deg)`;
+  const secondHand = document.querySelector(".second-hand");
+  secondHand.style.transform = `rotate(${secondsAngle}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesAngle = (minutes / 60) * 360 + 90;
+  const minutesHand = document.querySelector(".min-hand");
+  minutesHand.style.transform = `rotate(${minutesAngle}deg)`;
   console.log(seconds);
+
+  const hours = now.getHours();
+  const hoursAngle = (hours / 12) * 360;
+  const hoursHand = document.querySelector(".hour-hand");
+  hoursHand.style.transform = `rotate(${hoursAngle}deg)`;
 }
 
 setInterval(setDate, 1000);
